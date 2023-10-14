36b52d45d3411673bd384e495a13f773fc52737d
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-10-14 12:30:30 +1100

    Use inline syntax to provide types for the button title.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Button from "./component/Button";
 
 function App() {
   return (
-    <div className="App">
-      <div>Hi</div>
+    <div className="app">
+      <h2>Explore How To Use TypeScript in React </h2>
+      <Button title="Click Me" />
     </div>
   );
 }
---
+++ src/component/Button.tsx
@@ -0,0 +1,4 @@
+const Button = ({ title }: { title: string }) => {
+  return <button className="btn">{title}</button>;
+};
+export default Button;
